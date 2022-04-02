769b1c1dcab8a9368a9f3c0c21d3b50565f3ce7e
sonerxdev <sonerkaraevli@gmail.com>
2022-04-02 13:13:37 +0300

    Initial Commit

---
+++ Basic2ScreenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic2ScreenApp
+//
+//  Created by Soner Karaevli on 2.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
